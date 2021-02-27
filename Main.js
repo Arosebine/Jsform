249d6629f42e2a177b0d4a3677f7cc87e7b56877
Arowolo Ebine <79418698+Arosebine@users.noreply.github.com>
2021-02-27 22:05:43 +0100

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,107 @@
+// alert(10);
+// document.write('<hi> Hello world from Arowolo</h1>');
+
+// // single line comment
+// /*
+// */ 
+
+// /// variable
+// var name; // variable declaration
+// name = 'Favour'; // variable Assignment
+// var name = 'Lebrom James';
+// console.log (name)
+
+// let hobby = 'football';
+// hobby ='swimming';
+
+// console.log(hobby);
+
+// const gender = 'male';
+
+// let number1 = 10;
+// let number2 = 20;
+// let number3 = 30;
+
+
+// // string
+
+// console.log('i am at favour\'s place')
+
+
+
+
+// let firstname = 'John';
+// let lastname = 'Doe';
+// let age = '30';
+// let occupation = 'Programmer';
+// let ismarried = false;
+
+// // let description = 'my name is '+ firstname + ' ' + lastname +' and I am ' + age + ' ' + 'years old';
+
+// let description = `My name is ${firstname} ${lastname} and I am ${age} years old`;
+
+
+
+// console.log(description)
+
+
+// let sum = '11' + 10;
+// console.log(sum)
+
+// let convertedstring = parseInt('11')
+// let sum = convertedstring + 20;
+// console.log(sum)
+
+// console.log(11/10)
+// console.log(11%10)
+// console.log(10*10)
+// console.log(2**10)
+
+
+// // comparison operator
+
+// console.log(1>2);
+// console.log(2>=2);
+// console.log(2>=10);
+// console.log('2'==2);
+// console.log('3'===2);
+
+
+// // 
+
+// console.log(typeof isSweet);
+// console.log(typeof 10);
+// console.log(typeof 3.5);
+
+let firstname = 'Arowolo';
+let lastname = 'Ebine';
+let hobby = 'Sleeping';
+
+let description = `I am ${firstname} ${lastname} and I like eating, ${hobby}, drifting`
+console.log(description)
+
+
+// find the sum of the three percentage of 30%, 10%, 4%
+
+let percentage= percentage1 =30/100, percentage2 = 10/100, percentage3 = 4/100;
+console.log(percentage);
+
+
+
+
+4/100
+
+
+
+//what is the result of 5-2/3
+
+   // let a=true, b=false, c=4
+   
+   // what is the result of 
+   // (c>2) && (c<2)
+   // (b/a) && (b && b)
+
+
+let house = ['green', 'Yellow', 'White', 'Shalom', 'Glory'];
+console.log(house)
+console.log[house] = '3';
